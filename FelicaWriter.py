76a7f0b45089798efe3214d20a647a9466c6bd62
suic <66006042+Sui8@users.noreply.github.com>
2025-06-23 00:51:22 +0900

    Update FelicaWriter.py

--- FelicaWriter.py
+++ FelicaWriter.py
@@ -3,33 +3,36 @@ import binascii
 import time
 import logging
 
-syscode=0xFE00
-svccode=0x39C9
+syscode = 0xFE00
+svccode = 0x39C9
 logging.basicConfig()
-#logging.getLogger('nfc').setLevel(logging.DEBUG-1)
+# logging.getLogger("nfc").setLevel(logging.DEBUG-1)
 
 def on_connect(tag):
-  print ("on connect")
-  print (str(tag))
+  print("Connected")
+  print(str(tag))
   
   if isinstance(tag,nfc.tag.tt3.Type3Tag):
-    tag.idm,tag.pmm=tag.polling(system_code=syscode)
+    tag.idm, tag.pmm = tag.polling(system_code=syscode)
     tag.sys=syscode
+    
     try:
-      sc=nfc.tag.tt3.ServiceCode(svccode>>6,svccode&0x3F)
-      bc=nfc.tag.tt3.BlockCode(2)
-      data=tag.write_without_encryption([sc],[bc],bytearray(16))
-      print("wrote")
+      sc = nfc.tag.tt3.ServiceCode(svccode >> 6, svccode & 0x3F)
+      bc = nfc.tag.tt3.BlockCode(2)
+      data = tag.write_without_encryption([sc], [bc], bytearray(16))
+      print("Wrote")
+  
     except Exception as e:
-      print("exception:%s" % e)
+      print(f"Error: {e}")
   else:
-    print("not Type3Tag")
+    print("Error: Not Type3Tag")
+
   return True 
 
 def main():
-  print('start')
-  with nfc.ContactlessFrontend('usb') as clf:
-    clf.connect(rdwr={'on-connect':on_connect})
+  print("Program has started!")
+  with nfc.ContactlessFrontend("usb") as clf:
+    clf.connect(rdwr={"on-connect":on_connect})
 
-if __name__=='__main__':
+if __name__=="__main__":
   main()
