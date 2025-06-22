a08010594c72a2873d6e2da16873a20c9ba22370
suic <66006042+Sui8@users.noreply.github.com>
2025-06-23 00:52:41 +0900

    Update FelicaLiteSWriter.py

--- FelicaLiteSWriter.py
+++ FelicaLiteSWriter.py
@@ -4,7 +4,7 @@ service_code_9 = 0x0009
 service_code_b = 0x000b
     
 def on_connect(tag):
-  print("on connect")
+  print("Connected")
   # タグのIDなどを出力する
   print(str(tag))
     
@@ -13,19 +13,20 @@ def on_connect(tag):
       # ブロック0番の内容を16進数[bb]で書き込み、読み出す。
       sc = nfc.tag.tt3.ServiceCode(service_code_9 >> 6, service_code_9 & 0x3f)
       bc = nfc.tag.tt3.BlockCode(5, service=0)
-      #data = 16 * "\xcc"
+      # data = 16 * "\xcc"
       data = tag.write_without_encryption([sc], [bc], bytearray(16))
       print(("block{0}: {1}".format( 0, binascii.hexlify(tag.read_without_encryption([sc_9], [bc])))))
+
     except Exception as e:
-      print("error: %s" % e)
+      print(f"Error: {e}")
   else:
-    print("error: tag isn't Type3Tag")
+    print("Error: Tag isn't Type3Tag")
     
 # タッチ時のハンドラを設定して待機する
 def main():
-  print('start')
-  with nfc.ContactlessFrontend('usb') as clf:
-    clf.connect(rdwr={'on-connect':on_connect})
+  print("Program has started!")
+  with nfc.ContactlessFrontend("usb") as clf:
+    clf.connect(rdwr={"on-connect":on_connect})
 
-if __name__=='__main__':
+if __name__ == "__main__":
   main()
